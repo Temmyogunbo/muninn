0d9db6589dc75e1c505efd5e2b2ec63081477147
Emmanuel Temilola Ogunbo <Ogunbotemilola@yahoo.com>
2026-04-24 22:30:22 -0700

    Update scriptss

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,7 @@ echo "📂 Project root: $PROJECT_ROOT"
 
 # 1. Deploy infrastructure and Lambda functions
 echo "📦 Building infrastructure and deploying Lambda functions..."
-uv run deploy.py $ENVIRONMENT $PROJECT_NAME
+uv run $PROJECT_ROOT/scripts/deploy.py $ENVIRONMENT $PROJECT_NAME
 
 # 2. Final messages
 echo -e "\n✅ Deployment complete!"
