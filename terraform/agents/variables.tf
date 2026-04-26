1364cea7da5085efe6dfe676d1d0ed28f249e067
Emmanuel Temilola Ogunbo <Ogunbotemilola@yahoo.com>
2026-04-25 18:48:12 -0700

    Update programs

--- terraform/agents/variables.tf
+++ terraform/agents/variables.tf
@@ -16,12 +16,12 @@ variable "aurora_secret_arn" {
 variable "bedrock_model_id" {
   description = "Bedrock model ID to use for agents"
   type        = string
-  default    = "us-east-2"
 }
 
 variable "bedrock_region" {
   description = "AWS region for Bedrock"
   type        = string
+  default    = "us-east-2"
 }
 
 
